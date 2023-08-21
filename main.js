6322a8f00001f28aee5df382e09aaf256004322f
devhabib <habibr152@gmail.com>
2023-08-22 04:04:38 +0600

    header done, working on responsive

--- main.js
+++ main.js
@@ -7,31 +7,77 @@ document.addEventListener("DOMContentLoaded", function () {
   const boxes = document.querySelectorAll(".dropdown__box");
   const searchBtn = document.querySelector(".search__btn--box");
   const searchBox = document.querySelector(".search__input");
+  const main = document.querySelector(".main");
+  const mobileMenu = document.querySelector(".header__mobile--btn");
+  const headerMenu = document.querySelector(".header__navbar--menu");
+  const searchMenu = document.querySelector(".search__menu");
+  const searchClose = document.querySelector(".search__input--cross");
 
-  menuItems.forEach((menuItem) => {
-    menuItem.addEventListener("click", () => {
-      menuItems.forEach((item) => {
-        item.classList.remove("active");
-      });
-      boxes.forEach((box) => {
-        box.classList.remove("active");
-      });
-
-      menuItem.classList.add("active");
-      const correspondingBox = menuItem.querySelector(".dropdown__box");
-      correspondingBox.classList.add("active");
-    });
-  });
-
-  searchBtn.addEventListener("click", () => {
+  function removeActiveClasses() {
     menuItems.forEach((item) => {
       item.classList.remove("active");
     });
     boxes.forEach((box) => {
       box.classList.remove("active");
     });
+    main.classList.remove("overlay");
+    searchBox.classList.remove("active");
+    searchMenu.classList.remove("hidden");
+    headerMenu.classList.remove("active");
+  }
 
-    searchBtn.classList.add("active");
-    searchBox.classList.add("active");
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", () => {
+      // Toggle the active class for the clicked menu item and corresponding box
+      menuItem.classList.toggle("active");
+      const correspondingBox = menuItem.querySelector(".dropdown__box");
+      correspondingBox.classList.toggle("active");
+
+      if (menuItem.classList.contains("active")) {
+        // Add overlay to main when a menu is open
+        main.classList.add("overlay");
+        searchBox.classList.remove("active");
+      } else {
+        // Remove overlay when the menu is closed
+        main.classList.remove("overlay");
+      }
+    });
+  });
+
+  searchBtn.addEventListener("click", () => {
+    // Toggle the active class for the search button
+    searchBox.classList.toggle("active");
+
+    if (searchBox.classList.contains("active")) {
+      // Add overlay to main when the search is open
+      main.classList.add("overlay");
+      searchMenu.classList.add("hidden");
+      headerMenu.classList.remove("active");
+    } else {
+      // Remove overlay when the search is closed
+      main.classList.remove("overlay");
+    }
+  });
+
+  main.addEventListener("click", (event) => {
+    if (!event.target.closest(".search__menu--single")) {
+      removeActiveClasses();
+    }
+  });
+
+  mobileMenu.addEventListener("click", () => {
+    headerMenu.classList.toggle("active");
+    if (headerMenu.classList.contains("active")) {
+      // Add overlay to main when the mobile menu is open
+      main.classList.add("overlay");
+      searchBox.classList.remove("active");
+    } else {
+      // Remove overlay when the mobile menu is closed
+      main.classList.remove("overlay");
+    }
+  });
+
+  searchClose.addEventListener("click", () => {
+    removeActiveClasses();
   });
 });
